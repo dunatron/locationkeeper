c5a0cbe1ee67c2f5b3242527bd4227857c8bc49b
Heath Dunlop <heath.dunlop@samdog.nz>
2017-04-21 11:35:31 +1200

    refining search

---
+++ mysite/_config.php
@@ -0,0 +1,24 @@
+<?php
+
+global $project;
+$project = 'mysite';
+
+global $database;
+$database = 'locationkeeper_dev_db';
+
+require_once('conf/ConfigureFromEnv.php');
+
+// Set the site locale
+i18n::set_locale('en_US');
+
+FulltextSearchable::enable();
+Code::add_extension("FulltextSearchable('Title','Desc')");
+
+Solr::configure_server(array(
+    'host' => 'localhost',
+    'indexstore' => array(
+        'mode' => 'file',
+        'path' => BASE_PATH . '/solr'
+    ),
+    'port'  =>  '8983'
+));
