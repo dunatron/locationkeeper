24c3c27df363b2e91497ab3ba40086edcf68cb97
Heath Dunlop <heath.dunlop@samdog.nz>
2017-04-21 13:20:11 +1200

    refining search

--- themes/keeper/js/search/code-search.js
+++ themes/keeper/js/search/code-search.js
@@ -10,22 +10,34 @@ $(document).ready(function () {
     $(CodeSearchBtn).on('click', function(e){
         e.preventDefault();
 
-        var browserurl =   window.location.href,
-            url = browserurl,
-            keyword = $('#Form_CodeSearchForm_keyword').val();
+        // var browserurl =   window.location.href,
+        //     url = browserurl,
+        //     keyword = $('#Form_CodeSearchForm_keyword').val();
+        //
+        // //alert(url);
+        // $.ajax({
+        //     type:"POST",
+        //     url: url + '/searchCode',
+        //     data: {Keyword:keyword},
+        //     success: function (response) {
+        //         $('.search-results-wrapper').html(response);
+        //     },
+        //     complete: function(){
+        //
+        //     }
+        // });
 
-        //alert(url);
-        $.ajax({
-            type:"POST",
-            url: url + '/searchCode',
-            data: {Keyword:keyword},
-            success: function (response) {
+        var url = window.location.href + '/searchCode';
+
+        $.ajax(url)
+            .done(function (response) {
                 $('.search-results-wrapper').html(response);
-            },
-            complete: function(){
+            })
+            .fail (function (xhr) {
+                alert('Error: ' + xhr.responseText);
+            });
 
-            }
-        });
+
     });
 
 });
